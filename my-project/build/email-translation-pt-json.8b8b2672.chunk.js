4d24d5fd7876e5f9167771caefee3922fd2f1436
Adrian Wasielewski <adrian.wasielewski@avenga.com>
2022-09-26 09:21:12 +0200

    Remove build

--- my-project/build/email-translation-pt-json.8b8b2672.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_project=self.webpackChunkmy_project||[]).push([[3166],{18210:e=>{e.exports={}}}]);
--- my-project/build/email-translation-tr-json.eaa729bb.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmy_project=self.webpackChunkmy_project||[]).push([[2411],{45442:e=>{e.exports={}}}]);
